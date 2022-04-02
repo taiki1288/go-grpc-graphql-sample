c37611e0c201f0f50aa0195485c29df5e5b91736
taiki yamashita <taikity0808@gmail.com>
2022-04-02 14:51:21 +0900

    feat: メソッドの大枠を実装

--- article/service/service.go
+++ article/service/service.go
@@ -23,3 +23,23 @@ type service struct {
 func NewService(r repository.Repository) Service {
 	return &service{r}
 }
+
+func (s *service) CreateArticle(ctx context.Context, req *pb.CreateArticleRequest) (*pb.CreateArticleResponse, error) {
+	// 記事のCreateの処理を記述する
+}
+
+func (s *service) ReadArticle(ctx context.Context, req *pb.ReadArticleRequest) (*pb.ReadArticleResponse, error) {
+	// 記事のRead処理を記述する
+}
+
+func (s *service) UpdateArticle(ctx context.Context, req *pb.UpdateArticleRequest) (*pb.UpdateArticleResponse, error) {
+	//  記事のUpdate処理を記述する
+}
+
+func (s *service) DeleteArticle(ctx context.Context, req *pb.DeleteArticleRequest) (*pb.DeleteArticleResponse, error) {
+	// 記事のDelete処理を記述
+}
+
+func (s *service) ListArticle(req *pb.ListArticleRequest, stream pb.ArticleService_ListArticleServer) error {
+	// 記事の全取得処理を記述する
+}
