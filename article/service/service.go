fa02814bcd870d643d6291afc662d702b6c30c36
taiki yamashita <taikity0808@gmail.com>
2022-04-02 14:09:25 +0900

    feat: service/interfaceを実装

--- article/service/service.go
+++ article/service/service.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"context"
+
+	"github.com/taiki1288/go-grpc-graphql-sample/article/pb"
+)
+
+type Service interface {
+	CreateArticle(ctx context.Context, req *pb.CreateArticleRequest) (*pb.CreateArticleResponse, error)
+	ReadArticle(ctx context.Context, req *pb.ReadArticleRequest) (*pb.ReadArticleResponse, error)
+	UpdateArticle(ctx context.Context, req *pb.UpdateArticleRequest) (*pb.UpdateArticleResponse, error)
+	DeleteArticle(ctx context.Context, req *pb.DeleteArticleRequest) (*pb.DeleteArticleResponse, error)
+	ListArticle(req *pb.ListArticleRequest, stream pb.ArticleService_ListArticleServer) error
+}
