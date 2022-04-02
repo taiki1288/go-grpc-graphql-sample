b36b15f1553bc7590b4f94c4d2bd6bb5e3dc15d9
taiki yamashita <taikity0808@gmail.com>
2022-04-02 10:20:10 +0900

    feat: インターフェースを定義

--- article/repository/repository.go
+++ article/repository/repository.go
@@ -1,5 +1,17 @@
 package repository
 
 import (
+	"context"
+	"database/sql"
+
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/taiki1288/go-grpc-graphql-sample/article/pb"
 )
+
+type Repository interface {
+	InsertArticle(ctx context.Context, input *pb.ArticleInput) (int64, error)
+	SelectArticleByID(ctx context.Context, id int64) (*pb.Article, error)
+	UpdateArticle(ctx context.Context, id int64, input *pb.ArticleInput) error
+	DeleteArticle(ctx context.Context, id int64) error
+	SelectArticles() (*sql.Rows, error)
+}
